b70382e7ba2f1a4d0ff224ea99f441e656348224
Xuanwo <xuanwo.cn@gmail.com>
2016-09-21 00:19:37 +0800

    refactor mirror-web

--- gen_robot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '# robots.txt for https://mirrors.tuna.tsinghua.edu.cn' > robots.txt
-echo 'User-agent: *' >> robots.txt
-echo '' >> robots.txt
-
-curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
-	echo "Disallow: /${name}" >> robots.txt
-done
