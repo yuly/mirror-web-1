06375d5c9d99c789435696dfb599616c6fe5efc5
Xuanwo <xuanwo.cn@gmail.com>
2016-09-22 10:01:20 +0800

    update ci

---
+++ script/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+source /root/.bashrc
+cd /mnt/code/mirror-web && git pull
+/root/.pyenv/shims/python3 /mnt/code/mirror-web/script/genindex.py
+nvm use stable && /usr/local/bin/jekyll build -s /mnt/code/mirror-web -d /mnt/web --incremental  && cp -r /mnt/web/* /mnt/mirror
